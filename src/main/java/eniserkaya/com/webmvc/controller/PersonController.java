645acc7c12aa9ea3f552da8622a60fa33a5a824e
Enis Erkaya <enis.erkaya@intra.local>
2019-11-04 14:52:55 +0300

    lombok aktif edildi

---
+++ src/main/java/eniserkaya/com/webmvc/controller/PersonController.java
@@ -0,0 +1,20 @@
+package eniserkaya.com.webmvc.controller;
+
+import eniserkaya.com.webmvc.entity.Person;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @PostMapping("/addPerson")
+    public List<Person> addPerson(@RequestBody List<Person> person) {
+        person.forEach(p -> p.setYas(p.getYas() + 1));
+        return person;
+    }
+}
---
+++ src/main/java/eniserkaya/com/webmvc/entity/Person.java
@@ -0,0 +1,15 @@
+package eniserkaya.com.webmvc.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Person {
+    private String isim;
+    private String soyIsim;
+    private int yas;
+
+}
