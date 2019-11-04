71dca021ba42f68a1d407881340bf08689cdde82
Enis Erkaya <enis.erkaya@intra.local>
2019-11-04 15:07:48 +0300

    -

--- src/main/java/eniserkaya/com/webmvc/controller/PersonController.java
+++ src/main/java/eniserkaya/com/webmvc/controller/PersonController.java
@@ -1,6 +1,8 @@
 package eniserkaya.com.webmvc.controller;
 
 import eniserkaya.com.webmvc.entity.Person;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,12 @@ import java.util.List;
 public class PersonController {
 
     @PostMapping("/yasaBirEkle")
-    public List<Person> yasaBirEkle(@RequestBody List<Person> person) {
+    public ResponseEntity<List<Person>> yasaBirEkle(@RequestBody List<Person> person) {
         person.forEach(p -> p.setYas(p.getYas() + 1));
-        return person;
+
+        return ResponseEntity.ok()
+                .header("key","Value")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(person);
     }
 }
