d77eb1c4f82965b8a7314efceaa2301e91aba8ef
Enis Erkaya <enis.erkaya@intra.local>
2019-11-05 09:18:34 +0300

    global exception handler eklendi

--- src/main/java/eniserkaya/com/webmvc/entity/User.java
+++ src/main/java/eniserkaya/com/webmvc/entity/User.java
@@ -4,7 +4,6 @@ import eniserkaya.com.webmvc.validator.TCKimlikNo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
@@ -16,26 +15,32 @@ public class User {
 
     @NotBlank(message = "İsim boş bırakılamaz.")
     private String isim;
+
     @NotBlank
     private String soyIsim;
+
     @Min(value = 12)
     @Min(value = 100)
     private int yas;
+
     @Email
     @NotBlank
     private String email;
+
     @TCKimlikNo
     private String tcKimlikNo;
-    @DateTimeFormat(pattern = "dd/MM/yyyy") // This is for bind Date with @ModelAttribute
+
     @PastOrPresent
     private LocalDateTime dogumTarihi;
+
     @Size(max = 250)
     private String ikametgah;
+
     @NotBlank
     private String userName;
 
     @AssertTrue
-    public boolean isUserNamePublic(){
+    public boolean isUserNamePublic() {
         return !userName.equals("admin");
     }
 
