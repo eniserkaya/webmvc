725be1ff47fd552999ccbe9ea9a7761cfad3afa9
Enis Erkaya <enis.erkaya@intra.local>
2019-11-05 13:43:02 +0300

    http://localhost:8080/swagger-ui.html

--- src/main/java/eniserkaya/com/webmvc/SwaggerConfiguration.java
+++ src/main/java/eniserkaya/com/webmvc/SwaggerConfiguration.java
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-
+ //http://localhost:8080/swagger-ui.html arayuzunden rest api doc acilabilir
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
